6efe0cc6c4ffe24301e47a022c8a0de60f687d4e
Lucas Boinet <lucas@skyloud.fr>
2023-01-27 10:39:02 +0100

    categories list

--- src/views/CatalogCategory.vue
+++ src/views/CatalogCategory.vue
@@ -1,32 +1,26 @@
 <template>
   <span v-if="loading">loading</span>
-  <div v-else class="p-8 flex flex-col gap-8">
-    <a href="/catalog" class="underline text-[#2c00d5]">Retour</a>
+  <div v-else class="p-8 flex flex-col gap-8 items-start">
+    <a
+      href="/catalog"
+      class="underline bg-[#2c00d5] text-white rounded py-1 px-2"
+      >Retour</a
+    >
     <h1 class="text-3xl font-bold">Catalog - "{{ selectedCategory }}"</h1>
-    <div class="flex flex-col justify-start items-end gap-8">
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-7"
-      >
-        <Product
-          v-for="product in filteredProducts"
-          :key="product.id"
-          :data="product"
-        />
-      </div>
-    </div>
+    <ProductsList :products="filteredProducts" />
   </div>
 </template>
 
 <script>
 import { storeToRefs } from 'pinia'
-import Product from '@/components/Product.vue'
+import ProductsList from '@/components/ProductsList.vue'
 import { useProductsStore } from '@/stores/products.js'
 import { computed, onMounted, reactive, toRefs } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default {
   components: {
-    Product
+    ProductsList
   },
   setup() {
     const { products, loading } = storeToRefs(useProductsStore())
